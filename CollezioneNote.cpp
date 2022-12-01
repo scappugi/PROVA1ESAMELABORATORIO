05bc73b4b84b11978706bffdd22f31b256dc0465
Simone <simone.cappugi@stud.unifi.it>
2022-12-01 17:35:29 +0100

    COMMIT: aggiunti i notify e quasi completati i metodi di remove. (ancora da testare)

--- CollezioneNote.cpp
+++ CollezioneNote.cpp
@@ -9,13 +9,20 @@ void CollezioneNote::AddListToDo(Nota *nota) {
     if (nota->isInseritoInUnaLista() == 0) { //controllo che non sia già stato inserito in un altra lista
             nota->setInseritoInUnaLista(1);
             listaToDo.push_back(nota); //inserisco in una lista
+             notify();
     }
     else cout << "la seguente nota è gia stata inserita in un altra lista, non puoi riserirla anche qua"; //stampare i dettagli della lista
 }
 
 void CollezioneNote::removeToList(Nota *nota) {
-    listaToDo.remove(nota);
-    nota->setInseritoInUnaLista(0); //permette alla nota di essere riserita in una lista
+    for ( auto itr = listaToDo.begin() ; itr != listaToDo.end(); itr++){
+        if (*itr == nota){
+            listaToDo.remove(nota);
+            nota->setInseritoInUnaLista(0); //permette alla nota di essere riserita in una lista
+            notify();
+        }
+        else cout<<"il dato che stai provado ad eliminare non esiste in questa lista"<<endl;
+    }
 }
 void CollezioneNote::removeAndDestroyNote(Nota *nota) {
     listaToDo.remove(nota);
